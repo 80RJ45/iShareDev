ab87b53f7679a9f7e8e9c193d93ef097179ec347
Daniel Sánchez <danielmadrid@unah.hn>
2024-03-21 12:32:22 -0600

    prueba

---
+++ Sql/tablas y procedures.sql
@@ -0,0 +1,51 @@
+use atlas
+
+create table finca
+(
+	FincaID   int not null,
+	Codigo    varchar(20) not null,
+	Nombre    varchar(50) not null,
+	constraint pkFinca primary key (FincaID)
+)
+
+create table Lote
+(
+	LoteID     int not null,
+	FincaID    int not null,
+	Codigo     varchar(20) not null,
+	Productoid int not null,
+	constraint pkLote primary key (loteid),
+	constraint fkLoteFinca foreign key (FincaID) references finca,
+	constraint fkLoteProducto foreign key (ProductoID) references Producto
+)
+
+create table Producto
+(
+	ProductoID int not null,
+	Codigo     varchar(20)not null,
+	Nombre     varchar(20) not null,
+	constraint pkProducto primary key (ProductoID)
+)
+go
+
+alter procedure spProductoSelect @Productoid int
+as
+	select *from producto where ProductoID = @Productoid or ProductoID = 0
+go
+
+create procedure spProductoInsert @ProductoID int, @Codigo varchar(20), @Nombre varchar(20)
+as
+	select @ProductoID = isnull(max(productoid) + 1,0 ) from producto
+	insert into producto values (@ProductoID,@Codigo,@Nombre)
+go
+
+create procedure spProductoDelete @ProductoID int
+as
+	delete from producto where ProductoID = @ProductoID
+go
+
+create procedure spProductoUpdate @Productoid int,@Codigo varchar(20),@Nombre varchar(20)
+as
+	update producto set Codigo = @Codigo, Nombre = @Nombre where ProductoID = @Productoid
+go
+
