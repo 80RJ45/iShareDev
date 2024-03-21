92256386ff65fd6781d8497da8b26979996bdaf9
80RJ45 <guillermoborjas@gmail.com>
2024-03-21 11:00:53 -0600

    frmPrueba

---
+++ cDevelop/Forms/frmPruebaList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace cDevelop.Forms
+{
+    public partial class frmPruebaList : dcLibrary.dcParentList
+    {
+        public frmPruebaList()
+        {
+            InitializeComponent();
+        }
+    }
+}
