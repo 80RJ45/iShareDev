341b15b0d99c6e376edc125e38ad88a9f4a4d8f8
Daniel Madrid <danielmadrid@unah.hn>
2024-04-03 18:22:38 -0600

    aplicadas las políticas y antes de modificar el crear particiones

--- vbDevelop/Forms/frmActivoDetail.vb
+++ vbDevelop/Forms/frmActivoDetail.vb
@@ -1,4 +1,20 @@
-﻿Public Class frmActivoDetail
+﻿Imports System.Windows.Forms
+
+Public Class frmActivoDetail
+    Public Sub New(cnx As dcLibrary.dcConnect, parents As dcLibrary.dcParentList, DetailID As Integer)
+        MyBase.New(cnx, parents, DetailID)
+        ' Esta llamada es exigida por el diseñador.
+        InitializeComponent()
+
+        ' Agregue cualquier inicialización después de la llamada a InitializeComponent().
+        newDinamicTable(Connect, 0, "ActivoCab", "@ActivoCabID", "@ActivoCabID", DetailID, ParameterDirection.InputOutput, -1)
+        newDinamicTable(Connect, 1, "ActivoDet", "ActivoCabID", "ActivoCabID", DetailID, ParameterDirection.Input, 0)
+        LoadDinamicTables()
+    End Sub
+    Public Overrides Sub SetGrids()
+        MyBase.SetGrids()
+        dcGral.initGrid(dgvDetalle, dsGral.Tables("ActivoDet"), False, False, "", True, True, Connect, DataGridViewContentAlignment.MiddleCenter, True)
+    End Sub
     Private Sub frmActivoDetail_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
