8a99f57fcd1e3f621e5951ec15b21a912e5f79dc
80RJ45 <guillermoborjas@gmail.com>
2024-11-27 03:30:07 -0600

    Cambios Pendentes

---
+++ cDevelop/Cosecha/Forms/frmPrueba.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace cDevelop.Cosecha.Forms
+{
+    public partial class frmPrueba : dcLibrary.dcDetail
+    {
+        public frmPrueba()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ vbDevelop/Forms/Activos/frmActivoDepList.vb
@@ -0,0 +1,20 @@
+﻿Imports System.Data.SqlClient
+
+Public Class frmActivoDepList
+
+    Public Overrides Sub Init()
+        SQLGridParm = New SqlParameter("@ActivoDepID", 0)
+
+        Dim adp As New SqlDataAdapter
+
+        adp.SelectCommand = dcGral.getSQLCommand(Connect, "spActivoDepSelect", SQLGridParm)
+
+        Adaptador = adp
+        Detalle = True
+
+        MyBase.Init()
+    End Sub
+    Private Sub frmActivoDepList_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
